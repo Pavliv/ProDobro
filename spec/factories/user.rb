e6ebdcf103755da1dc759c513efa492a0d9ec540
mm1krob <mmikrob@gmail.com>
2018-04-18 23:12:31 +0300

    develop_PROD-88: delete phone_number from views and controller

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :user do
     name Faker::Name.first_name
     surname Faker::Name.last_name
-    phone_number Faker::Base.numerify('+380#########')
     email Faker::Internet.email
     password '369852'
     password_confirmation '369852'
